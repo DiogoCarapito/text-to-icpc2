34b5dc5c614e71c5fd8172202cbeb59504acbf1c
DiogoCarapito <diogo.carapito@icloud.com>
2024-09-16 22:43:13 +0100

    setup_cuda.sh update

--- setup_cuda.sh
+++ setup_cuda.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Create a .venv in python 3.11
+python3.11 -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
 # install requirements with make
 make all
 
@@ -7,10 +13,14 @@ make all
 pip uninstall -y torch torchvision torchaudio
 
 # Check which GPU is available
-nvcc --version
+#nvcc --version
 
 # Install torch for the available GPU
 pip install torch==2.1.1+cu121 torchvision==0.15.2+cu117 torchaudio==0.13.0+cu117 -f https://download.pytorch.org/whl/torch_stable.html
+
+# upgrade blinker
+pip install --upgrade --ignore-installed blinker
+pip show blinker
 
 # Final check if torch is working properly
 python -c "import torch; print(torch.__version__)"
